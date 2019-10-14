8acca96c9b5a15a9c824839845115157d6b5df24
Axel Demborg <axel.demborg@gmail.com>
2019-10-14 21:15:39 +0200

    update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import avatar from './avatar.jpg'
-import cppn from './cppn.png'
+import avatar from './img/avatar.jpg'
+import cppn from './img/cppn.png'
+import sju_ars_lycka from './img/sju_ars_lycka.jpg'
+import raytrace from './img/raytrace.png'
 
 interface Post {
   title: string;
@@ -51,7 +53,7 @@ class Grid extends React.Component<Page, {}> {
   render () {
     const {posts} = this.props
     return (
-      <div className="flex justify-center flex-wrap">
+      <div className="flex flex-wrap">
         {posts.map(
           post => 
         <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 p-4">
@@ -68,8 +70,9 @@ const App: React.FC = () => {
     <div className="container mx-auto">
       <Header/>
       <Grid posts={[
-        {title: "Vandalism", image: "test.jpg", intro: "bla bla bla", tags: ["Art", "Mirror"]},
-        {title: "CPPN", image: cppn, intro: "Generative art implemented in tensorflowJS", tags: ["Art", "Rendering"]}
+        {title: "Sju års lycka", image: sju_ars_lycka, intro: "Performing (very mild) civil disobedience by decorating a subway station with mirrors", tags: ["Art", "Mirror"]},
+        {title: "CPPN", image: cppn, intro: "Generative art implemented in tensorflowJS", tags: ["Art", "Rendering", "Coding"]},
+        {title: "Worst raytracer", image: raytrace, intro: "Builidng the worlds worst raytracer from scratch in Python", tags: ["Rendering", "Coding"]}
         ]}/>
     </div>
   );
