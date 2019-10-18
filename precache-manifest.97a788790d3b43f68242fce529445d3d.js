3c18b7e7e934d8fea682f1f2d43b0880fd763425
Axel Demborg <axel.demborg@gmail.com>
2019-10-18 10:15:21 +0200

    Updates

---
+++ precache-manifest.97a788790d3b43f68242fce529445d3d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945ba42f8c988df1cb533a797cc3e7c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f245884be226d6cdeb",
+    "url": "/static/css/main.0c6b5b59.chunk.css"
+  },
+  {
+    "revision": "8a3d99e13e58e7cf49fb",
+    "url": "/static/js/2.5fa8531e.chunk.js"
+  },
+  {
+    "revision": "e7f245884be226d6cdeb",
+    "url": "/static/js/main.20a0bd0c.chunk.js"
+  },
+  {
+    "revision": "38eb91d1434b1ccf6bb1",
+    "url": "/static/js/runtime-main.7c1bd79d.js"
+  },
+  {
+    "revision": "d50c9485feaae09423e556b7609a473b",
+    "url": "/static/media/avatar.d50c9485.jpg"
+  },
+  {
+    "revision": "4ce37009a85c9a292277340836afcd36",
+    "url": "/static/media/cppn.4ce37009.png"
+  },
+  {
+    "revision": "cfcaa5209ad4e5148eda2a54c3b571d3",
+    "url": "/static/media/neon.cfcaa520.jpg"
+  },
+  {
+    "revision": "b7face0f19d3b2b094ffa43899d8c3ba",
+    "url": "/static/media/raytrace.b7face0f.png"
+  },
+  {
+    "revision": "d8a56cce6df159647c0b378704200bdf",
+    "url": "/static/media/sju_ars_lycka.d8a56cce.jpg"
+  },
+  {
+    "revision": "a79701fe8e8f1667822f4f8b6105fefd",
+    "url": "/static/media/this_very_website.a79701fe.png"
+  }
+]);
