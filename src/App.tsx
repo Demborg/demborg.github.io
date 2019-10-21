d96a1c060783fcead3f5044b68c5a27ebad9ccb0
Axel Demborg <axel.demborg@gmail.com>
2019-10-21 21:42:19 +0200

    fixing links

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import sju_ars_lycka from './img/sju_ars_lycka.jpg'
 import raytrace from './img/raytrace.png'
 import this_very_website from './img/this_very_website.png'
 
+interface Link {
+  text: string;
+  url: string;
+}
+
 interface Post {
   title: string;
   image: string;
   intro: string;
   body?: string;
-  tags: string[];
+  links: Link[];
 }
 
 interface Page {
@@ -28,9 +33,20 @@ class Header extends React.Component {
   }
 }
 
+class LinkButton extends React.Component<Link, {}> {
+  render () {
+    const {text, url} = this.props;
+    return (
+      <a href={url}>
+        <span className="inline-block hover:bg-gray-700 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold hover:text-gray-200 text-gray-700 mr-2">{text}</span>
+      </a>
+    )
+  }
+}
+
 class Card extends React.Component<Post, {}> {
   render () {
-    const {title, image, intro, tags} = this.props;
+    const {title, image, intro, links} = this.props;
     return (
       <div className="rounded overflow-hidden shadow-lg">
         <img className="w-full" src={image} alt={image}/>
@@ -41,7 +57,7 @@ class Card extends React.Component<Post, {}> {
           </p>
         </div>
         <div className="px-6 py-4">
-          {tags.map(tag => <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">{tag}</span>)}
+          {links.map(link => <LinkButton text={link.text} url={link.url}/>)}
         </div>
       </div>
     )
@@ -56,7 +72,7 @@ class Grid extends React.Component<Page, {}> {
         {posts.map(
           post => 
         <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 hover:zoom-10 p-4">
-          <Card title={post.title} image={post.image} intro={post.intro} tags={post.tags}/>
+          <Card title={post.title} image={post.image} intro={post.intro} links={post.links}/>
         </div>
           )}
       </div>
@@ -69,10 +85,30 @@ const App: React.FC = () => {
     <div className="container mx-auto">
       <Header/>
       <Grid posts={[
-        {title: "This very webside", image: this_very_website, intro: "Continuing to learn frontend by building myself a little portfolio page", tags: ["Coding"]},
-        {title: "Sju års lycka", image: sju_ars_lycka, intro: "Performing (very mild) civil disobedience by decorating a subway station with mirrors", tags: ["Photography", "Mirror"]},
-        {title: "CPPN", image: cppn, intro: "Generative art implemented in tensorflowJS", tags: ["Rendering", "Coding"]},
-        {title: "Worst raytracer", image: raytrace, intro: "Building the worlds worst raytracer from scratch in Python", tags: ["Rendering", "Coding"]},
+        {
+          title: "This very webside", 
+          image: this_very_website, 
+          intro: "Continuing to learn frontend by building myself a little portfolio page", 
+          links: [{text: "Try", url: "https://demborg.se/"}, {text: "Source", url: "https://github.com/Demborg/demborg.github.io"}]
+        },
+        {
+          title: "Sju års lycka", 
+          image: sju_ars_lycka, 
+          intro: "Performing (very mild) civil disobedience by decorating a subway station with mirrors", 
+          links: [{text: "Instagram", url: "https://www.instagram.com/p/B3hWzMuFEOh/"}]
+        },
+        {
+          title: "CPPN",
+          image: cppn,
+          intro: "Generative art implemented in tensorflowJS",
+          links: [{text: "Try", url: "https://demborg.se/cppn/"}, {text: "Source", url: "https://github.com/Demborg/cppn"}]
+        },
+        {
+          title: "Worst raytracer",
+          image: raytrace,
+          intro: "Building the worlds worst raytracer from scratch in Python",
+          links: [{text: "Source", url: "https://github.com/Demborg/raytrace"}]
+        },
         ]}/>
     </div>
   );
