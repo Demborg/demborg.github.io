d88f13c66cfb5849981460f46301c6993230ad86
Axel Demborg <axel.demborg@volumental.com>
2025-09-21 22:38:51 +0200

    Add some styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: "Roboto Slab",
+                cssVariable: "--font-roboto-slab"
+            },
+            {
+                provider: fontProviders.google(),
+                name: "Great Vibes",
+                cssVariable: "--font-great-vibes"
+            },
+        ]
+    }
+});
